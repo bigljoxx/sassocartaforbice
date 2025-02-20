86cedea6b945f13f44c7fed05dc1099d1cd255f2
RiccardoMotta23 <riccardomotta23@gmail.com>
2025-02-20 14:41:04 +0100

    risultati.txt
    File che tiene conto delle vittorie/sconfitte e pareggi

--- funzioni_gioco.py
+++ funzioni_gioco.py
@@ -13,10 +13,16 @@ def gioco():
     print(f"Il computer ha scelto: {scelta_computer}")
 
     if scelta_utente == scelta_computer:
+        risultato = "Pareggio!"
         print("Pareggio!")
     elif (scelta_utente == "sasso" and scelta_computer == "forbice") or \
          (scelta_utente == "carta" and scelta_computer == "sasso") or \
          (scelta_utente == "forbice" and scelta_computer == "carta"):
+        risultato = "Vittoria!"
         print("Hai vinto!")
     else:
+        risultato = "Sconfitta!"
         print("Hai perso!")
+
+    with open("risultati.txt", mode = "a") as file:
+        file.write(risultato)
